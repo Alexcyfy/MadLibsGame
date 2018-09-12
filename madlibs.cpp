dd8ad0e815477282f7803c13e3ecfb355bd36848
Alexcyfy <acollanton@mail.csuchico.edu>
2018-09-12 16:32:45 -0700

    Madlibs Code

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,52 @@
 //Authors: 
 #include <iostream>
 #include <string>
-
+using namespace std; 
 int main()
 {
+string adj1;
+string noun1;
+int number1;
+string adj2;
+int number2;
+string adj3;
+string noun2;
+string noun3;
+
+std::cout<<"Enter an adjective :";
+std::cin>>adj1;
+std::cout<<"Enter a noun :";
+std::cin>>noun1;
+std::cout<<"Enter a number :";
+std::cin>>number1;
+std::cout<<"Enter an adjective :";
+std::cin>>adj2;
+std::cout<<"Enter a large number :";
+std::cin>>number2;
+std::cout<<"Enter a noun :";
+std::cin>>noun3;
+std::cout<<"Enter an adjective :";
+std::cin>>adj3;
+std::cout<<"Enter a noun :";
+std::cin>>noun2;
+cout<<"The ";
+cout<<adj1;
+cout<<" President of the country known for producing ";
+cout<<noun1;
+cout<<" has delcared a national holiday to honor their ";
+cout<<number1;
+cout<<" founders. There will be a ";
+cout<<adj2;
+cout<<" parade and ";
+cout<<noun3;
+cout<<" will be gifted to all the citizens, failure to attend will result in paying a fine of ";
+cout<<number2;
+cout<<" and possibily eaten alive by a ";
+cout<<adj3;
+cout<<" ";
+cout<<noun2;
+
+
 
   return 0;
 }
